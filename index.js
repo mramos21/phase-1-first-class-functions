d77756ae5d5921245ec7da92245d2095d056915e
Michael Ramos <ramosmichael@live.com>
2022-02-03 01:36:50 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    console.log(callback());
+}
+
+function returnsANamedFunction() {
+    return receivesAFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log('I\'m anonymous.')
+    }
+}
